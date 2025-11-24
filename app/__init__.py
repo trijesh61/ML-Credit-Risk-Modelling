9f9cf5372cbf7bdff0aacbe443ea0bf02ca2d87a
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-11-24 06:42:22 +0530

    updating

--- app/__init__.py
+++
@@ -1,1 +0,0 @@
-# Makes the 'app' directory a package for reliable imports on Streamlit Cloud.
--- m.py
+++
@@ -1,3 +0,0 @@
-import joblib
-model_data = joblib.load("artifacts/model_data.joblib")
-print(model_data.keys())
